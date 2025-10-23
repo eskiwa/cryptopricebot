f93abdff5784cf36c041bdc2ef34932eb1d711b5
xerea <harlybusinessmind@gmail.com>
2025-10-23 17:27:15 +0800

    Add files via upload
    add logging

--- bot.py
+++ bot.py
@@ -3,60 +3,21 @@ from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
+from datetime import datetime
+import traceback
 
 data = ''
-###
-# symbol = input("coin")
-
-# url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
-# parameters = {
-#         'symbol': f'{symbol.upper()}',   # You can change this to ETH, SOL, etc.
-#         'convert': 'USD'
-# }
-
-# headers = {
-#     'Accepts': 'application/json',
-#     'X-CMC_PRO_API_KEY': '3ba60acd46b94e928189c6a5155612f8',
-# }
-
-# session = requests.Session()
-# session.headers.update(headers)
-
-# try:
-#     response = session.get(url, params=parameters)
-#     data = json.loads(response.text)
-#     print(f"${data['data'][symbol.upper()]['quote']['USD']['price']}")
-# except (ConnectionError, Timeout, TooManyRedirects) as e:
-#         print(e)
-
-############################################################################################################
-
 bot_token = "8414940488:AAEDjdVdRKpHqLGuVX-QEjVrXJZ8oMp4QhM"
-# chat_id = "8250762387"
-# #message = f"${data['data'][symbol.upper()]['quote']['USD']['price']}"
-
-# boturl = f"https://api.telegram.org/bot{bot_token}/sendMessage"
-
-# payload = {
-#     "chat_id": chat_id,
-#     "text": message
-# }
-
-# response = requests.post(boturl, data=payload)
-
-# if response.status_code == 200:
-#     print("Message sent successfully!")
-# else:
-#     print("Failed to send message:", response.text)
-
-
-
 coin = ""
-
+ADMIN_ID = 8250762387
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     global coin
+    user = update.message.from_user
     coin = update.message.text  # Save to variable
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    log_message = f"📩 [{timestamp}]\nUser: @{user.username} ({user.id})\nMessage: {coin}"
+    await context.bot.send_message(chat_id=ADMIN_ID, text=log_message)
 
     url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
     parameters = {
@@ -90,7 +51,11 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
         else:
             await update.message.reply_text(f"${price:.10f}")
 
+async def error_handler(update: object, context: ContextTypes.DEFAULT_TYPE):
+    print("⚠️ An error occurred:")
+    print(traceback.format_exc())  # full traceback in Render logs
+
 app = ApplicationBuilder().token(bot_token).build()
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
-
+app.add_error_handler(error_handler)
 app.run_polling()
